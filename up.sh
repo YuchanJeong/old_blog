61771573c24cd9a64940f286d05f2e25be5161dc
YuchanJeong <wertypoiu@pusan.ac.kr>
2022-10-25 07:30:15 +0900

    rebuilding site Tue Oct 25 07:30:15 KST 2022

--- up.sh
+++ up.sh
@@ -6,23 +6,23 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # hugo -t <여러분의 테마>
 hugo -t congo
 
-# Go To Public folder, sub module commit
-cd public
-# Add changes to git.
-git add .
+# # Go To Public folder, sub module commit
+# cd public
+# # Add changes to git.
+# git add .
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
+# # Commit changes.
+# msg="rebuilding site `date`"
+# if [ $# -eq 1 ]
+#   then msg="$1"
+# fi
+# git commit -m "$msg"
 
-# Push source and build repos.
-git push origin main
+# # Push source and build repos.
+# git push origin main
 
-# Come Back up to the Project Root
-cd ..
+# # Come Back up to the Project Root
+# cd ..
 
 # blog 저장소 Commit & Push
 git add .
